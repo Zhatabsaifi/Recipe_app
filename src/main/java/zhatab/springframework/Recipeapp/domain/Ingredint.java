c507c2863df3257ff531094acdc743cca0830b29
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-08 22:24:00 +0530

    complete assignment .

---
+++ src/main/java/zhatab/springframework/Recipeapp/domain/Ingredint.java
@@ -0,0 +1,59 @@
+package zhatab.springframework.Recipeapp.domain;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+public class Ingredint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigInteger amount;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigInteger getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigInteger amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+}
