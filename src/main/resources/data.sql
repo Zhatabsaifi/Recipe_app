74b6f30cd238c544cee665da20fe275bbd94f31b
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-09 22:57:56 +0530

    Data initialize with spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into category (description) values ('Indian');
+insert into category (description) values ('American');
+insert into category (description) values ('Italian');
+insert into category (description) values ('Fast Food');
+
+insert into UNIT_OF_MEASURE (description) values ('Teaspoon');
+insert into UNIT_OF_MEASURE (description) values ('Tablespoon');
+insert into UNIT_OF_MEASURE (description) values ('Cup');
+insert into UNIT_OF_MEASURE (description) values ('Pinch');
+insert into UNIT_OF_MEASURE (description) values ('TableSalt');
+
