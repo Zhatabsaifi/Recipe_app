da08658929e547fd684871739709a65656f9152f
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-10 23:18:39 +0530

    RecipeService is use of list a recipe on index page

--- src/main/java/zhatab/springframework/Recipeapp/controller/IndexController.java
+++ src/main/java/zhatab/springframework/Recipeapp/controller/IndexController.java
@@ -1,31 +1,21 @@
 package zhatab.springframework.Recipeapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import zhatab.springframework.Recipeapp.domain.Category;
-import zhatab.springframework.Recipeapp.domain.UnitOfMeasure;
-import zhatab.springframework.Recipeapp.repositories.CategoryRepository;
-import zhatab.springframework.Recipeapp.repositories.UnitOfMeasureRepositry;
-
-import java.util.Optional;
+import zhatab.springframework.Recipeapp.services.RecipeService;
 
 @Controller
 public class IndexController {
+    private final RecipeService recipeService;
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepositry unitOfMeasureRepositry;
-
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepositry unitOfMeasureRepositry) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepositry = unitOfMeasureRepositry;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("Indian");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepositry.findByDescription("Cup");
-        System.out.println("Category id is: " + categoryOptional.get().getId());
-        System.out.println("UnitOfMeasur id is : " + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model){
+       model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/zhatab/springframework/Recipeapp/services/RecipeService.java
@@ -0,0 +1,9 @@
+package zhatab.springframework.Recipeapp.services;
+
+import zhatab.springframework.Recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
