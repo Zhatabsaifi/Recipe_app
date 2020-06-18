7c1e05d0e865ed83ccb0060e76dd86be1706f2b1
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-18 23:46:15 +0530

    project lombok use

--- src/main/java/zhatab/springframework/Recipeapp/controller/IndexController.java
+++ src/main/java/zhatab/springframework/Recipeapp/controller/IndexController.java
@@ -1,10 +1,12 @@
 package zhatab.springframework.Recipeapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import zhatab.springframework.Recipeapp.services.RecipeService;
 
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -15,6 +17,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting index Page");
        model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
--- src/main/java/zhatab/springframework/Recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/zhatab/springframework/Recipeapp/services/RecipeServiceImpl.java
@@ -1,11 +1,13 @@
 package zhatab.springframework.Recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import zhatab.springframework.Recipeapp.domain.Recipe;
 import zhatab.springframework.Recipeapp.repositories.RecipeRepository;
 
 import java.util.HashSet;
 import java.util.Set;
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -16,6 +18,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in service Recipe class");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
