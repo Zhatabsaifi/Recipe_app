b96742b33a86e2edbd473102d737060a5cbfd966
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-20 00:08:19 +0530

    Spring MockMvc

--- src/test/java/zhatab/springframework/Recipeapp/controller/IndexControllerTest.java
+++ src/test/java/zhatab/springframework/Recipeapp/controller/IndexControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import zhatab.springframework.Recipeapp.domain.Recipe;
 import zhatab.springframework.Recipeapp.services.RecipeService;
@@ -15,6 +17,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -54,5 +59,15 @@ class IndexControllerTest {
         verify(model,times(1)).addAttribute(eq("recipes"),arugumentCaptor.capture());
         Set<Recipe> recipeSet1 = arugumentCaptor.getValue();
         assertEquals(2,recipeSet1.size());
+    }
+
+
+    @Test
+    void testMockMvc() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
