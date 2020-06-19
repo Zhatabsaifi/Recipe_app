2910f58db89906b12defd3f307c9c23297b6a607
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-19 23:56:58 +0530

    Mackito Argument Capture in IndexController

--- src/test/java/zhatab/springframework/Recipeapp/controller/IndexControllerTest.java
+++ src/test/java/zhatab/springframework/Recipeapp/controller/IndexControllerTest.java
@@ -2,16 +2,19 @@ package zhatab.springframework.Recipeapp.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import zhatab.springframework.Recipeapp.domain.Recipe;
 import zhatab.springframework.Recipeapp.services.RecipeService;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -31,9 +34,25 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        // given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+        Recipe rr = new Recipe();
+        rr.setId(1L);
+        recipeSet.add(rr);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> arugumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String val = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index",val);
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),arugumentCaptor.capture());
+        Set<Recipe> recipeSet1 = arugumentCaptor.getValue();
+        assertEquals(2,recipeSet1.size());
     }
 }
