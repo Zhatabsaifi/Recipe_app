0155d1d2d03901f6d1f327f509d91429ea3ef77b
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-27 00:16:34 +0530

    thymeleaf add in show.html class

--- src/main/java/zhatab/springframework/Recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/zhatab/springframework/Recipeapp/bootstrap/RecipeBootstrap.java
@@ -142,6 +142,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/how_to_make_perfect_hard_boiled_eggs/");
+        guacRecipe.setServings(4);
+        guacRecipe.setSource("Simply Recipe");
+
         //add to return list
         recipes.add(guacRecipe);
 
