6dac89722e48478e189f1569309afe2a3cd1fc7e
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-06-30 23:43:04 +0530

    create Ingredient and test it.

--- src/main/java/zhatab/springframework/Recipeapp/controller/IngredientController.java
+++ src/main/java/zhatab/springframework/Recipeapp/controller/IngredientController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import zhatab.springframework.Recipeapp.commands.IngredientCommand;
+import zhatab.springframework.Recipeapp.commands.RecipeCommand;
+import zhatab.springframework.Recipeapp.commands.UnitOfMeasureCommand;
 import zhatab.springframework.Recipeapp.services.IngredientService;
 import zhatab.springframework.Recipeapp.services.RecipeService;
 import zhatab.springframework.Recipeapp.services.UnitOfMeasureService;
@@ -39,6 +41,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
