789605167c8ed17f3b745a79938e67af73383d9d
Zhatab Saifi <zhatabsaifi@gmail.com>
2020-07-02 00:26:01 +0530

    Show Image ,Add image , Change image

---
+++ src/main/java/zhatab/springframework/Recipeapp/services/ImageServiceImpl.java
@@ -0,0 +1,48 @@
+package zhatab.springframework.Recipeapp.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import zhatab.springframework.Recipeapp.domain.Recipe;
+import zhatab.springframework.Recipeapp.repositories.RecipeRepository;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+            log.debug("Successfully save image");
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
+    }
+}
